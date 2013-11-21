759822856878487e5461ec3309f9f4b6c06d0a82
calsaviour <calsaviour@gmail.com>
2013-11-21 23:35:24 +0800

    Modified zipline and added new data files

--- ziplineBackTest.py
+++ ziplineBackTest.py
@@ -1,19 +1,20 @@
-'''
-Created on Nov 15, 2013
-
-@author: Calvin
-'''
 from zipline import TradingAlgorithm
 from zipline.transforms import MovingAverage
+from zipline.finance.commission import PerTrade
 from zipline.utils.factory import load_from_yahoo
-from pandas.io.data import DataReader
+from datetime import datetime
+from zipline.finance.slippage import FixedSlippage
+from zipline.finance.performance import PerformancePeriod
+from zipline.finance.risk import RiskReport
 
 
-import pytz
-import matplotlib.pyplot as plt
-import datetime
+import zipline.finance.trading as trading
+import Lib.Utility as ut
 import os
 import pandas as pd
+import pytz
+import matplotlib.pyplot as plt
+import time
 
 class DualMovingAverage(TradingAlgorithm):
     """Dual Moving Average Crossover algorithm.
@@ -24,84 +25,260 @@ class DualMovingAverage(TradingAlgorithm):
     momentum).
 
     """
-    def initialize(self, short_window=100, long_window=400):
-        # Add 2 mavg transforms, one with a long window, one
-        # with a short window.
-        self.add_transform(MovingAverage, 'short_mavg', ['price'],
-                           window_length=short_window)
-
-        self.add_transform(MovingAverage, 'long_mavg', ['price'],
-                           window_length=long_window)
-
+    
+    
+    def initialize(self,tickers,basket):
+        
         # To keep track of whether we invested in the stock or not
+        
         self.invested = False
-
+        self.commission = PerTrade(0.00)
+        #self.starting_cash(10000)
+        self.set_slippage(FixedSlippage())
+        self.capital_base = 10000
+        self.tickers = tickers
+        self.basket = basket
+        
     def handle_data(self, data):
-        short_mavg = data['AAPL'].short_mavg['price']
-        long_mavg = data['AAPL'].long_mavg['price']
+        
+
+        KLCI_PerChange = data['KLCI_PerChange'].price
+        
+        
+        percent_changes = []    
+        
+        for ticker in tickers:
+            
+            percent_change = []
+            
+            if ticker == 'KLCI_Close':
+                continue 
+            
+            key = ticker
+            
+            ticker = ticker.replace('Close','PerChange')           
+            target = data[ticker].price
+            
+            if str(target) != 'nan':
+            
+                percent_change.append(key)
+                percent_change.append(target)
+                percent_changes.append(percent_change)
+
+        
+        #Look for the most minimum percentage change and invest
+        
+        temp = 0
+        ticker_PerChange = 0.00
+        for s in percent_changes:
+            
+            min = s[1]  
+            if min < temp:
+                temp = min 
+                ticker_PerChange = s[0]
+                ticker = s[0].replace('PerChange','Close')
+                
         buy = False
         sell = False
+        
+        if KLCI_PerChange < 0:
+            if ticker not in basket:
+                basket.append(ticker)
+                self.order(ticker, 1000,stop_price=None,limit_price=None)
+                buy = True
+                print "Buy " + ticker 
+            
 
-    # Has short mavg crossed long mavg?
-        if short_mavg > long_mavg and not self.invested:
-            self.order('AAPL', 100)
-            self.invested = True
-            buy = True
-            
-            #self.buy_orders.append(data['AAPL'].datetime)
-            print "{dt}: Buying 100 AAPL shares.".format(dt=data['AAPL'].datetime)
-            
-        elif short_mavg < long_mavg and self.invested:
-            self.order('AAPL', -100)
-            self.invested = False
-            sell = True
-            #self.sell_orders.append(data['AAPL'].datetime)
-            print "{dt}: Selling 100 AAPL shares.".format(dt=data['AAPL'].datetime)
+        for ticker in tickers: 
+            if ticker in basket:
+                ticker = ticker.replace('Close','PerChange')
+                ticker_PerChange = data[ticker].price                
+                if ticker_PerChange > 0.01:
+                    ticker = ticker.replace('PerChange','Close')
+                    basket.remove(ticker)
+                    self.order(ticker, -1000,stop_price=None,limit_price=None)
+                    sell = True
+                    print "Sold " + ticker
+
     # Record state variables. A column for each
     # variable will be added to the performance
     # DataFrame returned by .run()
-        self.record(short_mavg=short_mavg,
-                    long_mavg=long_mavg,
-                    buy=buy,
+        self.record(buy=buy,
                     sell=sell)
 
-# Load data
-#start = datetime(1990, 1, 1, 0, 0, 0, 0, pytz.utc)
-#end = datetime(2002, 1, 1, 0, 0, 0, 0, pytz.utc)
-#data = load_from_yahoo(stocks=['AAPL'], indexes={}, start=start,
-#                   end=end, adjusted=False)
 
+class greedySmart(TradingAlgorithm):
+    
+    '''A Strategy which uses ATR and manages risk'''
+    
+    def initialize(self,tickers,basket):
+        
+        self.invested = False
+        self.commission = PerTrade(0.00)
+        #self.starting_cash(10000)
+        self.set_slippage(FixedSlippage())
+        self.capital_base = 100000
+        self.tickers = tickers
+        self.basket = basket
+        
+    def handle_data(self, data):
+        
 
+        KLCI_PerChange = data['KLCI_PerChange'].price
+        
+        
+        percent_changes = []    
+        
+        for ticker in tickers:
+            
+            percent_change = []
+            
+            if ticker == 'KLCI_Close':
+                continue 
+            
+            key = ticker
+            
+            ticker = ticker.replace('Close','PerChange')           
+            target = data[ticker].price
+            
+            if str(target) != 'nan':
+            
+                percent_change.append(key)
+                percent_change.append(target)
+                percent_changes.append(percent_change)
 
+        
+        #Look for the most minimum percentage change and invest
+        
+        temp = 0
+        ticker_PerChange = 0.00
+        for s in percent_changes:
+            
+            min = s[1]  
+            if min < temp:
+                temp = min 
+                ticker_PerChange = s[0]
+                ticker = s[0].replace('PerChange','Close')
+                
+        buy = False
+        sell = False
+        
+        if KLCI_PerChange < 0:
+            if ticker not in basket:
+                basket.append(ticker)
+                self.order(ticker, 1000,stop_price=None,limit_price=None)
+                buy = True
+                print "Buy " + ticker 
+            
 
-# Run algorithm
+        for ticker in tickers: 
+            if ticker in basket:
+                ticker = ticker.replace('Close','PerChange')
+                ticker_PerChange = data[ticker].price                
+                if ticker_PerChange > 0.01:
+                    ticker = ticker.replace('PerChange','Close')
+                    basket.remove(ticker)
+                    self.order(ticker, -1000,stop_price=None,limit_price=None)
+                    sell = True
+                    print "Sold " + ticker
 
-data = pd.read_csv('C:\Users\Calvin\Documents\Aptana Studio 3 Workspace\Exercises\SAMPLE.csv',parse_dates=True,index_col=0)
+    # Record state variables. A column for each
+    # variable will be added to the performance
+    # DataFrame returned by .run()
+        self.record(buy=buy,
+                    sell=sell)
 
-data=pd.DataFrame(data['Close'],columns=['AAPL'])
-data=data.tz_localize('UTC')
-dma = DualMovingAverage()
-perf = dma.run(data)
+if __name__ == '__main__':
 
-print perf
+    start = datetime(2013, 1, 2, 0, 0, 0, 0, pytz.utc)
+    end = datetime(2013, 11, 14, 0, 0, 0, 0, pytz.utc)
+    
+    # Path for files
+    h = os.getcwd()
+    inputDir = h + '/Data/'
+    
+    KLSETickerfile = 'KLSETickers.txt'
+    filename = 'Chen_KLCI_Main'
+        
+    filepath = inputDir + KLSETickerfile
+    tickers = ut.readfile(filepath) 
+    
 
-# Plot results
-fig = plt.figure()
-ax1 = fig.add_subplot(211,  ylabel='Price in $')
-data['AAPL'].plot(ax=ax1, color='r', lw=2.)
-perf[['short_mavg', 'long_mavg']].plot(ax=ax1, lw=2.)
+    
+    filepath = inputDir + filename + '.csv'
+    ohlc = pd.read_csv(filepath,parse_dates=True,index_col=0)
+    #ohlc=ohlc.tz_localize('UTC')
+    
+    data = pd.DataFrame(index=ohlc.index)
+    
+    for ticker in tickers:    
+        data[ticker]=pd.DataFrame(ohlc[ticker])
+        ticker = ticker.replace('_Close',"")        
+        if ticker == 'KLCI':
+            data[ticker + '_PerChange'] = ohlc[ticker + '_Close']/ohlc[ticker + '_Close'].shift(1)-1
+            data[ticker + '_PerChange'] = data[ticker + '_PerChange'].shift(1)
+        else:
+            data[ticker + '_PerChange'] = ohlc[ticker + '_Close']/ohlc[ticker + '_Close'].shift(1)-1
+            data[ticker + '_PerChange'] = data[ticker + '_PerChange'].shift(1)
+        
+    data=data.tz_localize('UTC')
+    
+    basket = []
+    
+    '''index = [br.date for br in trading.environment.benchmark_returns]
+    rets = [br.returns for br in trading.environment.benchmark_returns]
+    bm_returns = pd.Series(rets, index=index).ix[start:end]
+    perf['benchmark_returns'] = (1 + bm_returns).cumprod().values
+    perf[['algorithm_returns', 'benchmark_returns']].plot(ax=ax1,
+                                                             sharex=True)
+    '''
+    
+    
+    dma = DualMovingAverage(tickers,basket)
+    perf = dma.run(data)
+    
+    bm = pd.DataFrame()
+    perf['algorithm_returns'] = (1 + perf.returns).cumprod()
+    
+    # Plot results
+    fig = plt.figure()
+    
+    ax1 = fig.add_subplot(311, ylabel='cumulative returns')
+    #perf[['algorithm_returns', 'benchmark_returns']].plot(ax=ax1,sharex=True)
+    perf['algorithm_returns'].plot(ax=ax1,sharex=True)
+        
+    ax2 = fig.add_subplot(312,  ylabel='Portfolio value in RM')
+    perf.portfolio_value.plot()
+    
+    ax2.plot(perf.ix[perf.buy].index, perf.portfolio_value[perf.buy],
+             '^', markersize=10, color='m')
+    ax2.plot(perf.ix[perf.sell].index, perf.portfolio_value[perf.sell],
+             'v', markersize=10, color='k')
+    
 
-ax1.plot(perf.ix[perf.buy].index, perf.short_mavg[perf.buy],
-         '^', markersize=10, color='m')
-ax1.plot(perf.ix[perf.sell].index, perf.short_mavg[perf.sell],
-         'v', markersize=10, color='k')
+    
+    
+    
+    sharpe = [risk['sharpe'] for risk in dma.risk_report['one_month']]
+    print "Monthly Sharpe ratios:", sharpe
+    
+    plt.tight_layout()
+    plt.legend(loc=0)
+    plt.gcf().set_size_inches(18, 8)
 
-ax2 = fig.add_subplot(212, ylabel='Portfolio value in $')
-perf.portfolio_value.plot(ax=ax2, lw=2.)
-
-ax2.plot(perf.ix[perf.buy].index, perf.portfolio_value[perf.buy],
-         '^', markersize=10, color='m')
-ax2.plot(perf.ix[perf.sell].index, perf.portfolio_value[perf.sell],
-         'v', markersize=10, color='k')
-
-plt.show()
+    # Save Report and Figure
+    outputname = time.strftime("%Y_%m_%d_%H_%M_%S")
+    h = os.getcwd()
+    
+    outputDir = h + '/Output/'
+    filename = outputDir + 'Report_' + outputname
+    perf.to_csv(filename,sep='\t',encoding='utf-8')
+    
+    filename = outputDir + 'Figure_' + outputname
+    plt.savefig(filename, dpi=400, bbox_inches='tight')
+    
+    
+    
+    
+    
+    
